7ecd8b9724b7d0d00433c89941ea095394e4335c
bingzhuwuhen <1209157727@qq.com>
2019-10-22 17:10:34 +0800

    项目第一天打卡

--- README.md
+++ README.md
@@ -9,4 +9,29 @@
 ## 用(传统方式)命令行把修改过的代码上传
 1. git add .
 2. git commit -m "提交信息"
-3. git push
+3. git 
+
+## 制作首页APP组件
+1. 完成Header区域，使用的是Mint-UI中的Header组件
+2. 制作底部的tabbar区域，使用的是MUI的Tabbar.html
+ + 在制作购物车小图标的时候，操作会相对多一些；
+ + 先把扩展图标的css样式，拷贝到项目中去；
+ + 在拷贝扩展图标的ttf文件，到项目中
+ + 为购物车小图标，添加如下样式"mui-icon mui-icon-extra mui-icon-extra-cart"
+3. 要在中间区域放置一个router-view来展示路由匹配到的组件
+
+## 改造tabbar 为router-link
+
+## 设置路由高亮，LinkActiveClass
+
+## 点击Tabbar中路由链接，展示对应的组件
+
+## 制作首页轮播图布局
+
+## 加载首页轮播图数据
+1. 获取数据，使用vue-resource
+2. 使用vue-resource的this.$http.get获取数据
+3. 获取到的数据保存到data上
+4. 使用v-for循环渲染每一个item项
+
+## 改造九宫格样式
--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 //入口文件
-console.log('OK')
 
 import Vue from 'vue'
 import app from './app.vue'
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+//1.2安装路由
+Vue.use(VueRouter)
+//1.3导入自己的router.js模块
+import router from './router.js'
+
+//2.1导入vue-resource
+import VueResource from 'vue-resource'
+//安装vue-resource
+Vue.use(VueResource)
 
 import { Header } from 'mint-ui';
+import { Swipe, SwipeItem } from 'mint-ui';
 
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 var vm=new Vue({
     el:'#app',
     data:{},
-    render:c=>c(app)
+    render:c=>c(app),
+    router//挂载路由到vm对象的实例上
 })
